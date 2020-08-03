262ac7d901dcbd62ccf9a377ac63a0ef76893378
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-08-03 23:37:52 +0530

    Added EJS Template

--- app.js
+++ app.js
@@ -3,14 +3,35 @@ const bodyParser = require('body-parser');
 
 app=express();
 
+app.set('view engine','ejs');
 app.set(express.static('static'));
+
 app.use(bodyParser.urlencoded(
   { extended:true }
 ));
 
+var items=["Buy Food","Cook Food","Eat Food"];
+
 app.get('/',function(req,res){
-    res.send("Hello World!");
-})
+    var today=new Date();
+    
+    var options={
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+      year: "numeric"
+    };
+
+    var day = today.toLocaleDateString("en-US",options);
+
+    res.render("list",{kindOfDay:day,newListItems:items});
+});
+
+app.post("/",function(req, res){
+  var item=req.body.item;
+  items.push(item);
+  res.redirect("/");
+});
 
 app.listen(3000,function(){
     console.log('Server started at port 3000');
